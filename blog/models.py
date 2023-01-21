bfe0f75172463eebaeae5dece663831486fb8db9
arrannis1800 <arrannis1800@gmail.com>
2023-01-22 00:02:38 +0200

    Add requirements.txt and updated models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
@@ -10,11 +12,19 @@ class Post(models.Model):
         verbose_name_plural = 'Posts'
 
     title = models.CharField(max_length=200, help_text='200 symbols max', db_index=True)
-    content = models.TextField(max_length=5000, blank=True, null=True, help_text='5000 symbols max')
+    content = RichTextField(max_length=5000, blank=True, null=True, help_text='5000 symbols max')
     date_created = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=50)
+    likes = models.ManyToManyField(User, related_name='postcomment', blank=True)
+    reply = models.ForeignKey('self', null=True, related_name='reply_ok', on_delete=models.CASCADE)
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
