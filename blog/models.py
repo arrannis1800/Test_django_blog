6d6f4240a1466ef34856e9a8e77313df0f869288
arrannis1800 <arrannis1800@gmail.com>
2023-01-21 23:59:54 +0200

    Revert "Add requirements.txt and updated models.py"
    This reverts commit 0f722ea5c99fd5a2111d798b6b507ada77f5a01b.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 from django.utils import timezone
-from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
@@ -12,20 +10,11 @@ class Post(models.Model):
         verbose_name_plural = 'Posts'
 
     title = models.CharField(max_length=200, help_text='200 symbols max', db_index=True)
-    # content = models.TextField(max_length=5000, blank=True, null=True, help_text='5000 symbols max')
-    content = RichTextField(max_length=5000, blank=True, null=True, help_text='5000 symbols max')
+    content = models.TextField(max_length=5000, blank=True, null=True, help_text='5000 symbols max')
     date_created = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=50)
-    likes = models.ManyToManyField(User, related_name='postcomment', blank=True)
-    reply = models.ForeignKey('self', null=True, related_name='reply_ok', on_delete=models.CASCADE)
-
-    def total_likes(self):
-        return self.likes.count()
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
